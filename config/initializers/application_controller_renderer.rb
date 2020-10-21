803a53e0977eeb669b7dab6befb6e81c564c273e
KEI <>
2020-10-21 23:55:39 +0900

    ベースページ作成

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'f83c0e4a2dda348b00f482b4f6aad6ae7d40f2303b567eec0b0f166c9f37ad53caa09feeb53d4dcbea35ddc7d6c4c539429ae2bd7c85c18e152b49aefdde7ce2'
